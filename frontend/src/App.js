c806f8c807b114748ed895befbe8000e0a5722f9
Caleb Thoennes <calebthelime@gmail.com>
2023-11-02 18:56:33 -0500

    added functional component Conversation2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Conversation from './components/Conversation';
+import Conversation2 from './components/Conversation2';
+
+function handleSubmit() {
+  alert("Submitted!")
+}
 
 function App() {
 
   return (
     <div className="App">
       <Conversation/>
+      <Conversation2 suspectName="Suspect" onSubmit={handleSubmit()}/>
     </div>
   );
 }
