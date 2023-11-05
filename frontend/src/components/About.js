9c4cf7a22f9407d15559b4644449440fefdcb515
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-11-04 22:03:23 -0500

    added main interface'

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -2,12 +2,16 @@ import React from "react";
 import Button from "./Button";
 import styles from "../styles/About.css"
 
+/**
+ * @returns a message about the developers in a div
+ */
 
-const About = () => {
+const About = ({setMode}) => {
+
 
     return(
         <div style={{backgroundColor: "rgb(153, 115, 76)", height: "100%", fontSize: "4em", paddingTop: "30px"}}>
-            <Button style={styles} id="back" text="back" ></Button>
+            <Button style={styles} id="back" text="back" setMode={setMode} setString={"main"}></Button>
             <strong>This is a game that seeks to leverage Meta's LLAMA 2</strong><br/>
             <strong>to create more realistic AI. This project was built for Mobile Computing</strong><br/>
             <strong> by the MMG team</strong>
--- frontend/src/components/MainPage.js
+++ frontend/src/components/MainPage.js
@@ -9,19 +9,20 @@ import styles from "../styles/MainMenu.css"
 const MainPage = () => {
 
     const [mode, setMode] = useState("main");
+    console.log(setMode)
     //responsible for the main menu 
     if (mode == "main") {
         return (
             <div id="main">
                 <strong id="txt" >murder mystery game</strong>
                 <Button style={styles} id="playButton" text="play"></Button>
-                <Button style={styles} id="aboutButton" text="About"></Button>
+                <Button style={styles} id="aboutButton" text="About" setMode={setMode} setString={"about"}></Button>
                 <img id="img" src="MainPageBkg.jpg" style={{ objectFit: 'cover', width: "100%", height: "100%" }}></img>
             </div>
         )
     }
     else if (mode == "about") {
-        return (<About></About>)
+        return (<About setMode={setMode}></About>)
     }
 
     
