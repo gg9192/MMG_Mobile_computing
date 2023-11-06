5368f0bd90ac34b712dee2d5fac949d2d4eb62c6
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-11-05 18:36:23 -0600

    wh;oi

--- frontend/src/components/GameUI.js
+++ frontend/src/components/GameUI.js
@@ -5,7 +5,8 @@ import SuspectCard from "./SuspectCard";
  */
 
 const GameUI = ({ }) => {
-    return (<div style={{display: "flex", width: "100%", height: "100%", flexDirection: "column", justifyContent: "space-between", backgroundColor: "rgb(153, 115, 76)"}}><SuspectCard imagePath={"./butler.png"} name="Butler"></SuspectCard>
+    return (<div style={{padding: "200px",display: "flex", width: "100%", height: "100%", flexDirection: "column", justifyContent: "space-between", backgroundColor: "rgb(153, 115, 76)"}}><SuspectCard imagePath={"./butler.png"} name="Butler" description="
+    In the grand manor of Ravenscroft, Mr. Montgomery is the esteemed butler, a model of unwavering professionalism and discretion. With his impeccably tailored attire, silver hair, and a demeanor that exudes grace and dignity, he oversees every detail of the household with unparalleled precision."></SuspectCard>
     </div>)
 }
 export default GameUI;
--- frontend/src/components/SuspectCard.js
+++ frontend/src/components/SuspectCard.js
@@ -1,15 +1,14 @@
 import React from "react";
-
+import styles from "../styles/SuspectCard.css"
 /**
  * @returns a card representing a suspect
  */
 const SuspectCard = ({name, imagePath, description}) => {
-    return (<div style={{height: "150px", width: "500px", border: "groove", borderRadius: "16px", 
-    display: "flex", alignItems: "center", background: "white", boxShadow: "10px 10px"}}>
+    return (<div style={styles} className="card">
         <img src={imagePath} style={{height: "80%"}}></img>
         <div style={{height: "80%"}}>
             <strong>{name}</strong>
-            <div>The butler for the Greybrook mansion. </div>
+            <div>{description}</div>
         </div>
     </div>)
 }
