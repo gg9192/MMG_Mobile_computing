986042fe3807ce5bae11a1d47d9fd91c45bd4d6b
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-11-05 20:52:57 -0600

    added img

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -4,7 +4,8 @@ import styles from "../styles/Button.css"
 /**
  * @prop {} Text the text to be displayed 
  * @prop {} id the id selector for css style
- * @prop {} setMode the setmode callback function that changes the mode
+ * @prop {} setMode the setmode callback function that changes the mode of whatever
+                thing that this is attatched to
  * @prop {} setString the string that the method sets the mode to
  * @returns a interface for the user to communicate with game characters
  */
