331b50c944c4afcf0d376d7d38978d2edd57cd7c
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-11-06 21:19:03 -0600

    added suspect selector

--- frontend/src/components/SuspectPanel.js
+++ frontend/src/components/SuspectPanel.js
@@ -3,9 +3,11 @@ import SuspectCard from "./SuspectCard";
 const SuspectPannel = ({ }) => {
     return (
         <div style={{backgroundColor: "rgb(153, 115, 76)", height: "100%", display: "flex", justifyContent: "center"}}>
-            
-            <div style={{display: "flex", width: "90%", flexDirection:  "row", alignItems: "center"}}>
-                <div style={{ display: "flex", width: "50%", height: "60%", flexDirection: "column", alignItems: "center", justifyContent: "space-between"}}>
+            <div>
+                <strong style={{position: "absolute", paddingLeft: "220px", paddingTop: "20px", fontSize: "3em"}}>Choose a suspect to interrogate</strong>
+            </div>
+            <div style={{display: "flex", width: "95%", flexDirection:  "row", alignItems: "center"}}>
+                <div style={{ display: "flex", width: "50%", height: "65%", flexDirection: "column", alignItems: "center", justifyContent: "space-between"}}>
                     <SuspectCard imagePath={"./butler.png"} name="Butler" description="In the grand manor of Ravenscroft, 
                     Mr. Montgomery is the esteemed butler, a model of unwavering professionalism and discretion. With his 
                     impeccably tailored attire, silver hair, and a demeanor that exudes grace and dignity, he oversees 
@@ -13,7 +15,7 @@ const SuspectPannel = ({ }) => {
                     <SuspectCard name="Lady Victoria"></SuspectCard>
                 </div>
                 <div style={{ alignItems: "center", justifyContent: "space-between", 
-                    display: "flex", width: "50%", height: "60%", flexDirection: "column"
+                    display: "flex", width: "50%", height: "65%", flexDirection: "column"
                 }}><SuspectCard imagePath={"./butler.png"} name="Butler"
                     description="In the grand manor of Ravenscroft, Mr. Montgomery is the esteemed butler, a model 
                     of unwavering professionalism and discretion. With his impeccably tailored attire, silver hair, 
