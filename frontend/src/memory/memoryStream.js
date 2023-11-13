123da03a1583751c43305e301b12fdfbca600f1e
Caleb Thoennes <calebthelime@gmail.com>
2023-11-13 00:29:08 -0600

    more comments

--- frontend/src/memory/memoryStream.js
+++ frontend/src/memory/memoryStream.js
@@ -22,9 +22,8 @@ class Memory {
 /**
  * Sends a completion to LLM asking to summarize multiple memories into one.
  * 
- * @param {MemoryStream} memStream
- * 
- * @returns {Memory} that that summarizes 
+ * @param {MemoryStream} memStream - a MemoryStream to summarize
+ * @returns {Memory} a Memory that summarizes the given memStream
  */
 function reflect(memStream) {
     //TODO: implement
@@ -33,10 +32,9 @@ function reflect(memStream) {
 /**
  * Will fetch a Memory of a MemoryStream by index number.
  * 
- * @param {MemoryStream} memStream 
- * @param {number} index - a non-negative integer representing 
- * 
- * @returns {Memory}
+ * @param {MemoryStream} memStream a MemoryStream to parse
+ * @param {number} index - a non-negative integer
+ * @returns {Memory} a Memory from memStream at the given index
  */
 function retrieveByIndex(memStream, index) {
     //TODO: implement
@@ -46,10 +44,9 @@ function retrieveByIndex(memStream, index) {
  * Will return a subset MemoryStream of the provided memStream from the startIndex
  * to the endIndex
  * 
- * @param {MemoryStream} memStream - the MemoryStream object to parse
+ * @param {MemoryStream} memStream - the MemoryStream to parse
  * @param {number} startIndex - a non-negative starting position to parse inclusively
  * @param {number} endIndex - a non-negative end position to parse inclusively
- * 
  * @returns {MemoryStream} 
  */
 function retrieveSubstream(memStream, startIndex, endIndex) {
