77a83e27d0292535f3fa571722a8524cec260526
Caleb Thoennes <calebthelime@gmail.com>
2023-11-14 15:37:45 -0600

    removed unnecessary functions from memoryStream

--- frontend/src/cognition/memoryStream.js
+++ frontend/src/cognition/memoryStream.js
@@ -8,37 +8,3 @@ class MemoryStream {
 
     }
 }
-
-/**
- * Sends a completion to LLM asking to summarize multiple memories into one.
- * 
- * @param {MemoryStream} memStream - a MemoryStream to summarize
- * @returns {Memory} a Memory that summarizes the given memStream
- */
-function reflect(memStream) {
-    //TODO: implement
-}
-
-/**
- * Will fetch a Memory of a MemoryStream by index number.
- * 
- * @param {MemoryStream} memStream a MemoryStream to parse
- * @param {number} index - a non-negative integer
- * @returns {Memory} a Memory from memStream at the given index
- */
-function retrieveByIndex(memStream, index) {
-    //TODO: implement
-}
-
-/**
- * Will return a subset MemoryStream of the provided memStream from the startIndex
- * to the endIndex.
- * 
- * @param {MemoryStream} memStream - the MemoryStream to parse
- * @param {number} startIndex - a non-negative starting position to parse inclusively
- * @param {number} endIndex - a non-negative end position to parse inclusively
- * @returns {MemoryStream} 
- */
-function retrieveSubstream(memStream, startIndex, endIndex) {
-    //TODO: implement
-}
