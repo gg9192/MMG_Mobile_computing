4b3defb02951929610927a50e40036f178d0c904
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-11-16 15:02:04 -0600

    i think this is fixed

--- frontend/src/components/GameIntro.js
+++ frontend/src/components/GameIntro.js
@@ -7,15 +7,17 @@ import Button from "./Button";
 */
 const GameIntro = ({setMode}) => {
 
-    return (<div style={{display: "flex", width: "100%", height: "100%", backgroundColor: "rgb(153, 115, 76)",
-    flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
-        <div style={{width: "80%"}}><strong style={{fontSize: "3em"}}>In the dimly lit, old manor of Greybrook, a dreadful murder has occurred. 
-        Sir Reginald Greybrook, the wealthy patriarch of the family, was found dead in 
-        his study. As the detective in charge, your task is to interview the four main 
-        suspects and uncover the truth behind this murder mystery.</strong>
-        <div style={{height: "80px", paddingLeft: "450px", marginTop: "-30px"}}>
+    return (<div style={{width: "100%", height: "100%", backgroundColor: "rgb(153, 115, 76)"}}>
+            <div style={{width: "100%", height: "70%", display: "flex", justifyContent: "center", alignContent: "center"}}>
+                <div style={{width: "90%", height: "90%"}}>
+                <strong style={{fontSize: "3.6vw", display: "flex", alignContent: "center", paddingTop: "30px"}}>In the dimly lit, old manor of Greybrook, a dreadful murder has occurred. 
+                Sir Reginald Greybrook, the wealthy patriarch of the family, was found dead in 
+                his study. As the detective in charge, your task is to interview the four main 
+                suspects and uncover the truth behind this murder mystery.</strong>
+                </div>
+            </div>
+        <div style={{height: "30%", display: "flex", justifyContent: "center", alignItems: "center"}}>
             <Button text="continue" setMode={setMode} setString={"main"}></Button>
-        </div>
         </div>
     </div>)
 }
