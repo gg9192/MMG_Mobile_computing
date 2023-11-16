d0ae8d8ee2caac6758f3a29183850d76c8978a34
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-11-16 17:14:46 -0600

    WHYWHYWHYWHYWHY

--- frontend/src/components/GameIntro.js
+++ frontend/src/components/GameIntro.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import { Textfit } from 'react-textfit';
 
 /** 
  * @prop {function} setMode setmode for GameUI
@@ -7,16 +8,16 @@ import Button from "./Button";
 */
 const GameIntro = ({setMode}) => {
 
-    return (<div style={{width: "100%", height: "100%", backgroundColor: "rgb(153, 115, 76)"}}>
-            <div style={{width: "100%", height: "70%", display: "flex", justifyContent: "center", alignContent: "center"}}>
-                <div style={{width: "90%", height: "90%"}}>
-                <strong style={{fontSize: "3.6vw", display: "flex", alignContent: "center", paddingTop: "30px"}}>In the dimly lit, old manor of Greybrook, a dreadful murder has occurred. 
-                Sir Reginald Greybrook, the wealthy patriarch of the family, was found dead in 
-                his study. As the detective in charge, your task is to interview the four main 
-                suspects and uncover the truth behind this murder mystery.</strong>
+    return (<div style={{width: "100%", height: "100%", backgroundColor: "rgb(153, 115, 76)", display: "flex", flexDirection: "column", justifyContent: "center"}}>
+            <div style={{width: "100%", display: "flex", justifyContent: "center", alignContent: "center"}}>
+                <div style={{width: "90%"}}>
+                <strong style={{fontSize: "2em"}}>In the dimly lit, old manor of Greybrook, a dreadful murder has occurred. 
+        Sir Reginald Greybrook, the wealthy patriarch of the family, was found dead in 
+        his study. As the detective in charge, your task is to interview the four main 
+        suspects and uncover the truth behind this murder mystery..</strong>
                 </div>
             </div>
-        <div style={{height: "30%", display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <div style={{display: "flex", height: "100px", justifyContent: "center", alignItems: "center"}}>
             <Button text="continue" setMode={setMode} setString={"main"}></Button>
         </div>
     </div>)
