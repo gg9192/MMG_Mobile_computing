33eccf81d80f53d2756d2deabe545b483d89ede9
gg9192 <42902579+gg9192@users.noreply.github.com>
2023-11-22 20:46:25 -0500

    renamed

--- backend/memory_search_ML_vector/memory_search/urls.py
+++ backend/memory_search_ML_vector/memory_search/urls.py
@@ -21,5 +21,5 @@ from memory_search.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', FetchMemories.as_view()),
+    path('API/fetchmemories', FetchMemories.as_view()),
 ]
